42e9b5ad631cb0962942c01734c04e23fabec11e
robdinges <robdinges@hotmail.com>
2020-10-11 19:55:17 +0200

    This version adds the code that was needed for Programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MakeCacheMatrix returns an object with all getters and setters for the 
+## inversed matrix. This returned object can be used as an input parameter by
+## CacheSolve to check for a cached version of the inversed matrix and return 
+## that or to update the cache 
 
-## Write a short comment describing this function
+## Create and return the list object including the setters and getters.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solved) m <<- solved
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## Check if the current cached inversed matrix is valid (not NULL) and otherwise
+## calculate it and cache it again, using the functions that were passed with
+## the input object
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m        
 }
+
